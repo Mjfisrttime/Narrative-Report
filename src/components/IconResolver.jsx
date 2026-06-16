c173c688b4f891a5416e0d23ac1efe102af64c27
Mjfisrttime <mistisojudyawon927@gmail.com>
2026-06-16 18:28:13 +0800

    feat: install react-icons and fix missing brand icons

--- src/components/IconResolver.jsx
+++ src/components/IconResolver.jsx
@@ -1,6 +1,19 @@
 import * as LucideIcons from 'lucide-react';
+import { FaFacebook, FaInstagram, FaXTwitter, FaGlobe } from 'react-icons/fa6';
+
+const BrandIcons = {
+  Facebook: FaFacebook,
+  Instagram: FaInstagram,
+  Twitter: FaXTwitter,
+  Globe: FaGlobe
+};
 
 export const IconResolver = ({ name, ...props }) => {
+  if (BrandIcons[name]) {
+    const BrandIcon = BrandIcons[name];
+    return <BrandIcon {...props} />;
+  }
+
   const Icon = LucideIcons[name];
   if (!Icon) return <LucideIcons.HelpCircle {...props} />;
   return <Icon {...props} />;
